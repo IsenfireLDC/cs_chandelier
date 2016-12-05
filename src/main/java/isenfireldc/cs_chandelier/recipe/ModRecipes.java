996523d4a7ad610fd3d6b52e8accb3d468d0eb42
Ben Klemp <isenfireldc@gmail.com>
2016-12-05 12:10:10 -0600

    ModRecipes.java: Changed Items.TORCH to Blocks.TORCH to fix crafting recipe

--- src/main/java/isenfireldc/cs_chandelier/recipe/ModRecipes.java
+++ src/main/java/isenfireldc/cs_chandelier/recipe/ModRecipes.java
@@ -1,14 +1,14 @@
 package isenfireldc.cs_chandelier.recipe;
 
 import isenfireldc.cs_chandelier.block.ModBlocks;
-import net.minecraft.init.Items;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 	
 	public static void init() {
-		GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.chandelier), "TTT", " T ", " T ", 'T', Items.TORCH);
+		GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.chandelier), "TTT", " T ", " T ", 'T', Blocks.TORCH);
 	}
 
 }
