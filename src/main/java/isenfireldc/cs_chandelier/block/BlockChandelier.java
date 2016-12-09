6ea79f84abbc109493175ac117d67e5aaf08bb65
Ben Klemp <isenfireldc@gmail.com>
2016-12-09 16:49:23 -0600

    Added explosion particles to chandelier block on activation

--- src/main/java/isenfireldc/cs_chandelier/block/BlockChandelier.java
+++ src/main/java/isenfireldc/cs_chandelier/block/BlockChandelier.java
@@ -1,9 +1,16 @@
 package isenfireldc.cs_chandelier.block;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockChandelier extends BlockBase {
 	
@@ -21,6 +28,12 @@ public class BlockChandelier extends BlockBase {
 	@Deprecated
 	public boolean isFullCube(IBlockState state) {
 		return false;
+	}
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+		worldIn.spawnParticle(EnumParticleTypes.EXPLOSION_HUGE, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 0.0D, 0.0D, 0.0D);
+		return true;
 	}
 	
 	/*@Override
